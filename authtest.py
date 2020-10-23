51d4e8200c04ecc1a9e1a2a200af46c87590460a
gurjot-kaur <47133703+gurjot-kaur@users.noreply.github.com>
2020-10-23 10:41:23 -0400

    Update authtest.py

--- authtest.py
+++ authtest.py
@@ -7,24 +7,30 @@ def sign_up_cognito(ci,upi,user,pwd):
         ClientId= ci,
         Username= user,
         Password= pwd)
-						 
-                         
+						                          
     cidp.admin_confirm_sign_up(
         UserPoolId= upi,
         Username= user)
 
-    r = cidp.admin_initiate_auth(
+    r = cidp.admin_initiate_auth( #first login 
         UserPoolId= upi,
         ClientId= ci,
         AuthFlow= "ADMIN_NO_SRP_AUTH",
         AuthParameters= {
         "USERNAME": user,
         "PASSWORD": pwd
+        })   
+        
+    m = cidp.admin_initiate_auth( #further logins
+        UserPoolId= upi,
+        ClientId= ci,
+        AuthFlow= "REFRESH_TOKEN_AUTH",
+        AuthParameters= {
+        "REFRESH_TOKEN" : r["AuthenticationResult"]["RefreshToken"]
         })
 	
-    return(r["AuthenticationResult"]["IdToken"])
-
-print("both key " + sign_up_cognito("c5f6c663b8iao253ldma34hg3","us-east-1_EOLt1gUue","b","pass123"))#both
+    return(m["AuthenticationResult"]["IdToken"])
+     
 
 print("add key " + sign_up_cognito("o2rjrh1h2snpr7c4o8od020n3","us-east-1_K7UQ0DJz9","b","pass123"))#add
 
